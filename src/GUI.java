87ca653cbb0ae6db92b7039ab3ca9d81a3312b6f
Tom Watson <thomasw2701@gmail.com>
2022-02-01 23:42:26 +0000

    Tweaked panel's GridLayout to display text better, and updated 'reset' and 'increment' methodss so that the panel displaying number of clicks appears inside the button following the user's initial click

--- src/GUI.java
+++ src/GUI.java
@@ -7,21 +7,20 @@ public class GUI {
 
     private static final JFrame frame = new JFrame();
     private static final JPanel panel = new JPanel();
-    private static final JButton clickButton = new JButton("Click me!");
-    private static final JButton resetButton = new JButton("Reset the click-count :D");
+    private static final JButton clickButton = new JButton("Click to start the test!");
+    private static final JButton resetButton = new JButton("Reset clicks");
     private static final JLabel label = new JLabel("Number of clicks: 0");
     private int count;
 
     public GUI() {
         panel.setBorder(BorderFactory.createEmptyBorder(300, 400, 100, 400));
-        panel.setLayout(new GridLayout(0, 1, 0, 5));
+        panel.setLayout(new GridLayout(2, 1, 0, 5));
         panel.setBackground(Color.gray);
         panel.add(clickButton);
         panel.add(resetButton);
         panel.add(label);
-//        panel.setMinimumSize(panel.getMinimumSize());
 
-        frame.add(panel, BorderLayout.CENTER);
+        frame.add(panel);
         frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
         frame.setTitle("Tom's Click Test");
         frame.pack();
@@ -31,18 +30,16 @@ public class GUI {
         clickButton.setFont(new Font("Calibri", Font.BOLD, 20));
         clickButton.addActionListener(increment);
         clickButton.setSize(new Dimension(100, 50));
+        clickButton.add(label, BorderLayout.CENTER);
 
         resetButton.setFocusable(false);
         resetButton.addActionListener(reset);
         resetButton.setFont(new Font("Calibri", Font.PLAIN, 20));
-        resetButton.setMinimumSize(resetButton.getMinimumSize());
-        resetButton.setPreferredSize(new Dimension(100, 50));
 
+        label.setVisible(false);
         label.setVerticalAlignment(JLabel.CENTER);
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setFont(new Font("Calibri", Font.PLAIN, 20));
-        label.setMinimumSize(label.getMinimumSize());
-        label.setPreferredSize(new Dimension(100, 50));
     }
 
     public static void main(String[] args) {
@@ -53,7 +50,8 @@ public class GUI {
         @Override
         public void actionPerformed(ActionEvent e) {
             count = 0;
-            label.setText("Number of clicks: " + count);
+            label.setVisible(false);
+            clickButton.setText("Click to start the test!");
         }
     };
 
@@ -61,7 +59,9 @@ public class GUI {
         @Override
         public void actionPerformed(ActionEvent e) {
             count++;
+            label.setVisible(true);
             label.setText("Number of clicks: " + count);
+            if (count >= 1) { clickButton.setText(""); }
         }
     };
 }
